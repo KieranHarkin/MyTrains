f2714a2271debd561ae43ac31af1f2128a73160f
Kieran Harkin <kieranharkin@googlemail.com>
2017-04-28 23:48:16 +0100

    Setting up project

---
+++ MyTrains.Core/App.cs
@@ -0,0 +1,26 @@
+﻿using MvvmCross.Core.ViewModels;
+using MvvmCross.Platform.IoC;
+
+namespace MyTrains.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            CreatableTypes()
+                .EndingWith("Repository")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            RegisterAppStart(new AppStart());           
+        }
+
+    }
+}
---
+++ MyTrains.Core/AppStart.cs
@@ -0,0 +1,13 @@
+﻿using MvvmCross.Core.ViewModels;
+using System;
+
+namespace MyTrains.Core
+{
+    public class AppStart : IMvxAppStart
+    {
+        public void Start(object hint = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
