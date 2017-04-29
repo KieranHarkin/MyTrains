23e0387f9a479469ac864d20ea090f2581bfee57
Kieran Harkin <kieranharkin@googlemail.com>
2017-04-30 00:32:39 +0100

    Adding ViewModels and ViewModel contracts. Added some extension methods

---
+++ MyTrains.Core/Models/App/MenuItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MvvmCross.Core.ViewModels;
+using MyTrains.Core.Utility;
+
+namespace MyTrains.Core.Models.App
+{
+    public class MenuItem : MvxViewModel
+    {
+        private bool _isSelected;
+
+        public string Title { get; set; }
+        public Type ViewModelType { get; set; }
+        public MenuOption Option { get; set; }
+        public bool IsSelected {
+            get {
+                return _isSelected;
+            }
+            set {
+                _isSelected = value;
+                RaisePropertyChanged(() => IsSelected);
+            }
+        }
+    }
+}
